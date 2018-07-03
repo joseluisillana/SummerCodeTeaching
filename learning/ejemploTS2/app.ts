8a3481c6a354bc102130d3e4498116d0ffff7e24
joseluis.illana <joseluis.illana@bbva.com>
2018-07-03 20:54:31 +0200

    Ejemplo 2 y 3

---
+++ learning/ejemploTS2/app.ts
@@ -0,0 +1,10 @@
+/**
+ Scope de variables (var)
+**/
+var mensaje1 = "hola";
+
+if (true) {
+	 var mensaje1 = "adios";
+}
+
+console.log(mensaje1);
---
+++ learning/ejemploTS3/app.ts
@@ -0,0 +1,10 @@
+/**
+ Scope de variables (let)
+**/
+let mensaje1 = "hola pepe";
+
+if (true) {
+	 let mensaje1 = "adios";
+}
+
+console.log(mensaje1);
